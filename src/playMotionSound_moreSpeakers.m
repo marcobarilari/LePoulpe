82e758784301339fb3f10ef5a207182278899ac1
marcobarilari <marcobarilr@gmail.com>
2022-03-11 19:44:26 +0100

    generate new pink noise each segment

--- src/playMotionSound_moreSpeakers.m
+++ src/playMotionSound_moreSpeakers.m
@@ -122,9 +122,14 @@ function playMotionSound_moreSpeakers(opt, axis, speakerIdx, soundArray, nbRepet
           
           nbSpeakersOn = nbSpeakersOn - 1;
           
-      end
-          
-          
+      end      
+      
+      outSound = {makePinkNoise(size(soundArray{1}, 2)), ...
+          makePinkNoise(size(soundArray{1}, 2)), ...
+          makePinkNoise(size(soundArray{1}, 2)), ...
+          makePinkNoise(size(soundArray{1}, 2)), ...
+          makePinkNoise(size(soundArray{1}, 2)), ...
+          makePinkNoise(size(soundArray{1}, 2))};
       
       segmentArray = soundArray(iSpeaker:(iSpeaker + nbSpeakersOn - 1));
       
@@ -208,5 +213,29 @@ function playMotionSound_moreSpeakers(opt, axis, speakerIdx, soundArray, nbRepet
     clear AO;
 
   end
+
+end
+
+function outSound = makePinkNoise(stimDuration)
+
+% let's start with white noise
+outSound = randn(1, stimDuration );
+
+% create q vector for filtering
+pinkFilter = zeros(1, length(outSound));
+pinkFilter(1) = 1;
+for i = 2:length(outSound)
+    pinkFilter(i) = (i - 2.5) * pinkFilter(i - 1) / (i - 1);
+end
+
+%filter the white noise
+outSound = filter(1, pinkFilter, outSound);
+
+% limit amplitude to [-1 to 1] aka normalize again
+outSound = outSound/max(outSound);
+outSound = outSound/abs(min(outSound));
+
+% apply amp to avoid chirping
+outSound =  0.95 .* outSound;
 
 end
