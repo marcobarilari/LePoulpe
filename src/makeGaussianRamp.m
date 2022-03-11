dfbbaddf6b26c02a9c90c21825bd4c63e4289764
marcobarilari <marcobarilr@gmail.com>
2022-03-11 17:57:06 +0100

    add func to compute gaussian function

---
+++ src/makeGaussianRamp.m
@@ -0,0 +1,21 @@
+% (C) Copyright 2022 CPP LePoulpe developers
+
+function gaussianRamp = makeGaussianRamp(bellLength)
+
+bellLenghtOneTail = bellLength / 2;
+
+STD = (bellLenghtOneTail / 10) * 4;
+
+MEAN = 0;
+
+x = -bellLenghtOneTail:1:bellLenghtOneTail;
+
+gaussianRamp = (1 / ( STD * sqrt(2 * pi))) * exp(-0.5 * ((x - MEAN) / STD) .^ 2);
+
+gaussianRamp = gaussianRamp / max(gaussianRamp);
+
+% figure(2)
+
+% plot(x,f);
+
+end
